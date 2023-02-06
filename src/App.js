5cc073abd47716afe52ce179c53c3511e3bac43c
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-02-06 14:48:17 -0800

    changes for support help

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { StyledComponent } from 'styled-components';
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,11 +11,32 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState('The Force is Missing');
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/`)
+      .then(res => {
+        setCharacters(res.data)
+      })
+      .catch(err => {
+        debugger;
+      })
+  }, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characters.map((char, idx) => {
+          return <Character char={char} key={idx} />
+        })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+
+
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+import App from "../App";
+
+const Character = ({ char }) => {
+    return (
+        <h2>
+            {char.name} {char.birth_year} {char.gender}
+        </h2>
+    )
+}
+
+
+
+
+export default Character;
